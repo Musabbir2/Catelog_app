bc4aefffd8541b8c6bc2abffa2b2f58895aa11b8
Musabbir Hossain Chowdhury <19103225@iubat.edu>
2023-05-19 14:19:14 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(FirstApp());
+}
+
+
+class FirstApp extends StatelessWidget {
+  const FirstApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     home: Material(
+      child:Center(
+        child: Container(
+          child: Text("Musabbir is creating his own"),
+        ),
+      ),
+     ),
+    );
+  }
+}
+
